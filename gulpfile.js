2a117eaf4bcfc0b18bb981fe3bc2ca0a59259989
pkv1979 <ACMilan1979@gmail.com>
2020-03-04 17:07:58 +0200

    feat: add gulp-sass into gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,35 @@
-const gulp        = require('gulp');
+const { src, dest, watch } = require('gulp');
 const browserSync = require('browser-sync').create();
-const reload      = browserSync.reload;
 const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
+const sass = require('gulp-sass');
 
 // Static server
-gulp.task('browser-sync',() => {
-    browserSync.init({
-        server: {
-          baseDir: "./src"
-        }
-    });
+function bs () {
+  serveSass();
+  browserSync.init({
+    server: {
+      baseDir: "./src"
+    }
+  });
 
-    gulp.watch("./src/*.html").on("change", reload);
-});
+  watch("./src/*.html").on("change", browserSync.reload);
+  watch("./src/scss/**/*.scss").on("change", serveSass);
+  watch("./src/js/*.js").on("change", browserSync.reload);
+};
 
-gulp.task('minify-css', () => {
-  return gulp.src('./src/css/*.css')
+function minifyCss () {
+  return src('./src/css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./src/css'));
-});
+    .pipe(dest('./src/css'));
+};
 
-gulp.task('default', gulp.parallel('minify-css', 'browser-sync'));
+function serveSass () {
+  return src("./src/scss/*.scss")
+    .pipe(sass())
+    .pipe(dest("./src/css"))
+    .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
