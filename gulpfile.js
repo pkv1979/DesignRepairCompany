b30d3e2e849659b4b58e5b951cede3b29691b5d8
pkv1979 <ACMilan1979@gmail.com>
2020-03-05 14:02:01 +0200

    feat: add gulp auto-prefixer in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 // Static server
 function bs () {
@@ -16,20 +17,23 @@ function bs () {
   watch("./src/*.html").on("change", browserSync.reload);
   watch("./src/scss/**/*.scss", serveSass);
   watch("./src/js/*.js").on("change", browserSync.reload);
-};
+}
 
 function minifyCss () {
   return src('./src/css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename({suffix: '.min'}))
     .pipe(dest('./src/css'));
-};
+}
 
 function serveSass () {
   return src("./src/scss/*.scss")
     .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false
+    }))
     .pipe(dest("./src/css"))
     .pipe(browserSync.stream());
-};
+}
 
 exports.serve = bs;
