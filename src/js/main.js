bd69121fac8909b5243d8e919c8a750e1e9fbf74
pkv1979 <ACMilan1979@gmail.com>
2020-03-16 10:13:50 +0200

    Добавил закрытие модального окна по клавише Esc и по клику вне формы

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,26 @@ document.addEventListener('DOMContentLoaded', (e) => {
   const modal = document.querySelector('.modal');
   const modalClose = modal.querySelector('.modal__close');
   const modalBtn = document.querySelectorAll('[data-toggle=modal');
+
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
-  }
+  };
 
   modalBtn.forEach(btn => {
     btn.addEventListener('click', switchModal);
   });
 
   modalClose.addEventListener('click', switchModal);
+
+  window.addEventListener('keydown', (e) => {
+    if (e.keyCode === 27 && modal.classList.contains('modal--visible')) {
+      switchModal();
+    }
+  });
+
+  modal.addEventListener('click', (e) => {
+    if (e.target.classList.contains('modal--visible')) {
+      switchModal();
+    }
+  });
 });
