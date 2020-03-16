ee9338a15325f1de43d0169b5b2e665da1248e79
pkv1979 <ACMilan1979@gmail.com>
2020-03-16 14:44:48 +0200

    Переписал скрипт модального окна с чистого JavaScript на jQuery.

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,63 @@
-document.addEventListener('DOMContentLoaded', (e) => {
-  const modal = document.querySelector('.modal');
-  const modalClose = modal.querySelector('.modal__close');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal');
+// // JavaScript Code
+// document.addEventListener('DOMContentLoaded', (e) => {
+//   const modal = document.querySelector('.modal');
+//   const modalClose = modal.querySelector('.modal__close');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal');
 
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  };
+//   const switchModal = () => {
+//     modal.classList.toggle('modal--visible');
+//   };
 
-  modalBtn.forEach(btn => {
-    btn.addEventListener('click', switchModal);
+//   // Открытие модального окна.
+//   modalBtn.forEach(btn => {
+//     btn.addEventListener('click', switchModal);
+//   });
+
+//   // Закрытие модального окна при назатии на 'крестик'
+//   modalClose.addEventListener('click', switchModal);
+
+//   // Закрытие модального окна при нажатии клавиши Esc (keyCode = 27).
+//   window.addEventListener('keydown', (e) => {
+//     if (e.keyCode === 27 && modal.classList.contains('modal--visible')) {
+//       switchModal();
+//     }
+//   });
+
+//   // Закрытие модального окна при назатии мышью за пределами диалога.
+//   modal.addEventListener('click', (e) => {
+//     if (e.target.classList.contains('modal--visible')) {
+//       switchModal();
+//     }
+//   });
+// });
+
+// jQuery Code
+$(document).ready(() => {
+  const modal = $('.modal');
+  const modalClose = $('.modal__close');
+  const modalBtn = $('[data-toggle=modal');
+
+  // Открытие модального окна.
+  modalBtn.on('click', () => {
+    modal.addClass('modal--visible');
   });
 
-  modalClose.addEventListener('click', switchModal);
+  // Закрытие модального окна при назатии на 'крестик'
+  modalClose.on('click', () =>{
+    modal.removeClass('modal--visible');
+  });
 
-  window.addEventListener('keydown', (e) => {
-    if (e.keyCode === 27 && modal.classList.contains('modal--visible')) {
-      switchModal();
+  // Закрытие модального окна при нажатии клавиши Esc (keyCode = 27).
+  $(window).on('keydown', (e) => {
+    if (e.keyCode === 27) {
+      modal.removeClass('modal--visible');
     }
   });
 
-  modal.addEventListener('click', (e) => {
-    if (e.target.classList.contains('modal--visible')) {
-      switchModal();
+  // Закрытие модального окна при назатии мышью за пределами диалога.
+  modal.on('click', (e) => {
+    if ($(e.target).hasClass('modal--visible')) {
+      modal.removeClass('modal--visible');
     }
   });
 });
