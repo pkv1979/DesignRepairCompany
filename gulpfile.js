4ebd5032e26b73878e98adbf51d91651efb6e0fd
pkv1979 <ACMilan1979@gmail.com>
2020-03-23 17:07:48 +0200

    Подготовка папки dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
-const rename = require("gulp-rename");
+const minify = require('gulp-minify');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const htmlmin = require('gulp-htmlmin');
+const tinypng = require('gulp-tinypng-compress');
 
 // Static server
 function bs () {
@@ -19,11 +21,55 @@ function bs () {
   watch("./src/js/*.js").on("change", browserSync.reload);
 }
 
-function minifyCss () {
-  return src('./src/css/*.css')
+function buildCss (done) {
+  src('./src/css/**/**.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(dest('./src/css'));
+    .pipe(dest('./dist/css'));
+  done();
+}
+
+function buildJs(done) {
+  src(['./src/js/**.js', '!./src/js/**.min.js'])
+    .pipe(minify({ext: {
+      min: '.js'
+    }
+    }))
+    .pipe(dest('./dist/js'));
+  src('./src/js/**.min.js')
+    .pipe(dest('./dist/js'));
+  done();
+}
+
+function buildHTML(done) {
+  src('./src/**.html')
+  .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('./dist'));
+  done();
+}
+
+function buildPHP(done) {
+  src('./src/**.php')
+    .pipe(dest('./dist'));
+  src('./src/phpmailer/**/**')
+    .pipe(dest('./dist/phpmailer'));
+  done();
+}
+
+function buildFonts(done) {
+  src('./src/fonts/**/**')
+    .pipe(dest('./dist/fonts'));
+  done();
+}
+
+function buildImg(done) {
+  src('./src/img/**/**')
+    .pipe(tinypng({
+      key: 'qSmSjKG5l11YgLCTsNMlsyqFmBr7x81F',
+      // sigFile: 'images/.tinypng-sigs',
+      // log: true
+    }))
+    .pipe(dest('./dist/img'));
+  done();
 }
 
 function serveSass () {
@@ -37,3 +83,4 @@ function serveSass () {
 }
 
 exports.serve = bs;
+exports.build = series(buildCss, buildJs, buildHTML, buildPHP, buildFonts, buildImg);
--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,3 @@
-// // JavaScript Code
-// document.addEventListener('DOMContentLoaded', (e) => {
-//   const modal = document.querySelector('.modal');
-//   const modalClose = modal.querySelector('.modal__close');
-//   const modalBtn = document.querySelectorAll('[data-toggle=modal');
-
-//   const switchModal = () => {
-//     modal.classList.toggle('modal--visible');
-//   };
-
-//   // Открытие модального окна.
-//   modalBtn.forEach(btn => {
-//     btn.addEventListener('click', switchModal);
-//   });
-
-//   // Закрытие модального окна при назатии на 'крестик'
-//   modalClose.addEventListener('click', switchModal);
-
-//   // Закрытие модального окна при нажатии клавиши Esc (keyCode = 27).
-//   window.addEventListener('keydown', (e) => {
-//     if (e.keyCode === 27 && modal.classList.contains('modal--visible')) {
-//       switchModal();
-//     }
-//   });
-
-//   // Закрытие модального окна при назатии мышью за пределами диалога.
-//   modal.addEventListener('click', (e) => {
-//     if (e.target.classList.contains('modal--visible')) {
-//       switchModal();
-//     }
-//   });
-// });
-
 // jQuery Code
 $(document).ready(() => {
   const modal = $('.modal');
